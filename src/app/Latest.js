6cce62df59bf62268f574cfce9ac6541fd3bb30a
Jose Rodriguez <jose.rodriguez@newtech.com.ar>
2018-12-27 17:53:29 -0300

    Testing with Cards

--- src/app/Latest.js
+++ src/app/Latest.js
@@ -13,20 +13,26 @@ const sortOptions = [
 
 const LatestCard = (props) => {
     return (
-        <div key={props._id} className="row card hoverable" style={{ borderRadius: 12 }}>
-            <div>
-                <span className="col s2"><img src={require('../public/Ana.jpg')} style={{width: 100, height: "auto"}}></img></span>
-                <span className="col s4"> Nombre y gastos aqui</span>
-                <span className="col s2"> Espacio adicional aqui</span>
-                <span className="col s2"> fecha y lugar aqui</span>
+        <div className="container center-align row" style={{ borderRadius: 12 }}>
+            <div className="row">
+                <div className="col s2">COLUMNA CENTRALIZADORA</div>
+                <div className="col s8 hoverable">
+                    <div className=" row">
+                        <span className="col s2"><img src={require('../public/Ana.jpg')} style={{ width: 100, height: "auto" }}></img></span>
+                        <span className="col s4"> Nombre y gastos aqui</span>
+                        <span className="col s2"> Espacio adicional aqui</span>
+                        <span className="col s2"> fecha y lugar aqui</span>
+                    </div>
+                </div>
+                <div className="col s2">}COLUMNA CENTRALIZADORA</div>
             </div>
-            <br/>
-            {/* <div style={{ margin: "10px" }}>
-                <h6>{props.name}</h6>
-                <p className="truncate">{props.description}</p>
-                <p name="amount">{props.ammount}</p>
-            </div> */}
         </div>
+
+        // {/* <div style={{ margin: "10px" }}>
+        //         <h6>{props.name}</h6>
+        //         <p className="truncate">{props.description}</p>
+        //         <p name="amount">{props.ammount}</p>
+        //     </div> */}
     );
 };
 
@@ -35,15 +41,18 @@ const Latest = (props) => {
     return (
         <div style={{ backgroundColor: "#00838f" }}>
             <NavBar />
-            <div className="container col s10 offset-s1 center-align white cyan-text text-darken-3" style={{ borderRadius: 12 }}>
-                <div>
-                    <h4>Ultimos Gastos</h4>
-                    {/* <Select className="col s2"
+            <div className="container center-align white cyan-text text-darken-3" style={{ borderRadius: 12 }}>
+            <h4 className="row">Ultimos Gastos</h4>
+                <div className="row">
+                    <div className="col s12 card-panel center-align">
+
+                        {/* <Select className="col s2"
                         // onChange={(selectedOption) => {this.setState({ newEntry: selectedOption.value })}}
                         options={sortOptions}
                         required>
                         </Select> */}
-                    {props.Recent.map(Expenses => <LatestCard key={props._id}  {...Expenses} />)}
+                        {props.Recent.map(Expenses => <LatestCard className="card offset-s2" key={props._id}  {...Expenses} />)}
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/app.js
+++ src/app/app.js
@@ -10,15 +10,15 @@ import Latest from './Latest'
 const App = () => {
 
     const RecentExpenses = [
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 1', ammount:'5000', _id: '00001' },
-        { name: 'Ana', place:'Urquiza', description:'gasto de prueba 2', ammount:'530', _id: '00002' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 3', ammount:'2000', _id: '00003' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 4', ammount:'2000', _id: '00004' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 5', ammount:'2000', _id: '00005' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 6', ammount:'2000', _id: '00006' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 7', ammount:'2000', _id: '00007' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 8', ammount:'2000', _id: '00008' },
-        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 9', ammount:'2000', _id: '00009' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 1', ammount:'5000', id: '00001' },
+        { name: 'Ana', place:'Urquiza', description:'gasto de prueba 2', ammount:'530', id: '00002' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 3', ammount:'2000', id: '00003' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 4', ammount:'2000', id: '00004' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 5', ammount:'2000', id: '00005' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 6', ammount:'2000', id: '00006' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 7', ammount:'2000', id: '00007' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 8', ammount:'2000', id: '00008' },
+        { name: 'Jose', place:'Urquiza', description:'gasto de prueba 9', ammount:'2000', id: '00009' },
 
 
     ];
